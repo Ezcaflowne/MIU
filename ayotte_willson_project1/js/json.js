32ee61b0560cd505903498d3aa5730e1ac474ce8
Ezcaflowne <ezcaflowne@fullsail.edu>
2012-08-28 19:38:46 -0700

    Added Feature, Search, Browse, and Title Sections

--- ayotte_willson_project1/js/json.js
+++ ayotte_willson_project1/js/json.js
@@ -37,5 +37,186 @@ var json = {
 		"isWhiteLock" 	: ["Whitelock?", ""],
 		"condition" 	: ["Condition:", "50"],
 		"comments"		: ["Additional Comments:", "Resident would like apartment walked prior to move-out date."]
+	},
+	"apartment4": {
+		"aptType" 		: ["Type:", "Vacant Available"],
+		"aptNum"  		: ["Apartment Number:", "2003"],
+		"aptSize" 		: ["Apartment Size:", "1x1"],
+		"vacDate" 		: ["Vacate:", "9/1/2012"],
+		"rdyDate" 		: ["Ready:", "9/8/2012"],
+		"isPower" 		: ["Power?", "Yes"],
+		"isWhiteLock" 	: ["Whitelock?", "Yes"],
+		"condition" 	: ["Condition:", "50"],
+		"comments"		: ["Additional Comments:", "Needs countertops resurfaced."]
+	},"apartment5": {
+		"aptType" 		: ["Type:", "Notice Available"],
+		"aptNum"  		: ["Apartment Number:", "1104"],
+		"aptSize" 		: ["Apartment Size:", "1x1"],
+		"vacDate" 		: ["Vacate:", "9/28/2012"],
+		"rdyDate" 		: ["Ready:", ""],
+		"isPower" 		: ["Power?", ""],
+		"isWhiteLock" 	: ["Whitelock?", ""],
+		"condition" 	: ["Condition:", "50"],
+		"comments"		: ["Additional Comments:", ""]
+	},"apartment6": {
+		"aptType" 		: ["Type:", "Vacant Rented"],
+		"aptNum"  		: ["Apartment Number:", "1119"],
+		"aptSize" 		: ["Apartment Size:", "2x2"],
+		"vacDate" 		: ["Vacate:", "9/10/2012"],
+		"rdyDate" 		: ["Ready:", "9/22/2012"],
+		"isPower" 		: ["Power?", "Yes"],
+		"isWhiteLock" 	: ["Whitelock?", "Yes"],
+		"condition" 	: ["Condition:", "20"],
+		"comments"		: ["Additional Comments:", "Sinks need to be replaced in shared bathroom."]
+	},"apartment7": {
+		"aptType" 		: ["Type:", "Vacant Available"],
+		"aptNum"  		: ["Apartment Number:", "2123"],
+		"aptSize" 		: ["Apartment Size:", "2x2"],
+		"vacDate" 		: ["Vacate:", "9/1/2012"],
+		"rdyDate" 		: ["Ready:", "9/8/2012"],
+		"isPower" 		: ["Power?", "Yes"],
+		"isWhiteLock" 	: ["Whitelock?", "Yes"],
+		"condition" 	: ["Condition:", "40"],
+		"comments"		: ["Additional Comments:", "Heavy carpet clean."]
+	},"apartment8": {
+		"aptType" 		: ["Type:", "Vacant Rented"],
+		"aptNum"  		: ["Apartment Number:", "2001"],
+		"aptSize" 		: ["Apartment Size:", "1x1"],
+		"vacDate" 		: ["Vacate:", "8/27/2012"],
+		"rdyDate" 		: ["Ready:", "9/10/2012"],
+		"isPower" 		: ["Power?", "Yes"],
+		"isWhiteLock" 	: ["Whitelock?", "Yes"],
+		"condition" 	: ["Condition:", "75"],
+		"comments"		: ["Additional Comments:", "Dishwasher will need to be replaced with new one."]
+	},"apartment9": {
+		"aptType" 		: ["Type:", "Notice Available"],
+		"aptNum"  		: ["Apartment Number:", "1007"],
+		"aptSize" 		: ["Apartment Size:", "2x2"],
+		"vacDate" 		: ["Vacate:", "9/27/2012"],
+		"rdyDate" 		: ["Ready:", "10/2/2012"],
+		"isPower" 		: ["Power?", ""],
+		"isWhiteLock" 	: ["Whitelock?", ""],
+		"condition" 	: ["Condition:", "50"],
+		"comments"		: ["Additional Comments:", "This is a transfer and needs to be done ASAP."]
+	},"apartment10": {
+		"aptType" 		: ["Type:", "Vacant Available"],
+		"aptNum"  		: ["Apartment Number:", "1009"],
+		"aptSize" 		: ["Apartment Size:", "2x2"],
+		"vacDate" 		: ["Vacate:", "9/1/2012"],
+		"rdyDate" 		: ["Ready:", "9/8/2012"],
+		"isPower" 		: ["Power?", ""],
+		"isWhiteLock" 	: ["Whitelock?", "Yes"],
+		"condition" 	: ["Condition:", "16"],
+		"comments"		: ["Additional Comments:", "Heavy Paint, Heavy Clean, New Carpet and Vinyl."]
+	},
+	"apartment11": {
+		"aptType" 		: ["Type:", "Vacant Rented"],
+		"aptNum"  		: ["Apartment Number:", "2104"],
+		"aptSize" 		: ["Apartment Size:", "1x1"],
+		"vacDate" 		: ["Vacate:", "8/27/2012"],
+		"rdyDate" 		: ["Ready:", "9/7/2012"],
+		"isPower" 		: ["Power?", "Yes"],
+		"isWhiteLock" 	: ["Whitelock?", "Yes"],
+		"condition" 	: ["Condition:", "100"],
+		"comments"		: ["Additional Comments:", "Apartment is completed."]
+	},
+	"apartment12": {
+		"aptType" 		: ["Type:", "Notice Available"],
+		"aptNum"  		: ["Apartment Number:", "1098"],
+		"aptSize" 		: ["Apartment Size:", "2x2"],
+		"vacDate" 		: ["Vacate:", "9/15/2012"],
+		"rdyDate" 		: ["Ready:", ""],
+		"isPower" 		: ["Power?", ""],
+		"isWhiteLock" 	: ["Whitelock?", ""],
+		"condition" 	: ["Condition:", "50"],
+		"comments"		: ["Additional Comments:", "Resident would like apartment walked prior to move-out date."]
+	},
+	"apartment13": {
+		"aptType" 		: ["Type:", "Vacant Available"],
+		"aptNum"  		: ["Apartment Number:", "1047"],
+		"aptSize" 		: ["Apartment Size:", "3x2"],
+		"vacDate" 		: ["Vacate:", "9/2/2012"],
+		"rdyDate" 		: ["Ready:", "9/9/2012"],
+		"isPower" 		: ["Power?", "Yes"],
+		"isWhiteLock" 	: ["Whitelock?", "Yes"],
+		"condition" 	: ["Condition:", "75"],
+		"comments"		: ["Additional Comments:", "This is a trasnfer, needed ASAP."]
+	},
+	"apartment14": {
+		"aptType" 		: ["Type:", "Vacant Available"],
+		"aptNum"  		: ["Apartment Number:", "1067"],
+		"aptSize" 		: ["Apartment Size:", "3x2"],
+		"vacDate" 		: ["Vacate:", "8/27/2012"],
+		"rdyDate" 		: ["Ready:", "9/14/2012"],
+		"isPower" 		: ["Power?", "Yes"],
+		"isWhiteLock" 	: ["Whitelock?", "Yes"],
+		"condition" 	: ["Condition:", "90"],
+		"comments"		: ["Additional Comments:", "Minor maintenance needed."]
+	},
+	"apartment15": {
+		"aptType" 		: ["Type:", "Notice Available"],
+		"aptNum"  		: ["Apartment Number:", "2043"],
+		"aptSize" 		: ["Apartment Size:", "3x2"],
+		"vacDate" 		: ["Vacate:", "9/30/2012"],
+		"rdyDate" 		: ["Ready:", ""],
+		"isPower" 		: ["Power?", ""],
+		"isWhiteLock" 	: ["Whitelock?", ""],
+		"condition" 	: ["Condition:", "50"],
+		"comments"		: ["Additional Comments:", "Resident would like apartment walked prior to transfer."]
+	},
+	"apartment16": {
+		"aptType" 		: ["Type:", "Vacant Available"],
+		"aptNum"  		: ["Apartment Number:", "2119"],
+		"aptSize" 		: ["Apartment Size:", "2x2"],
+		"vacDate" 		: ["Vacate:", "9/3/2012"],
+		"rdyDate" 		: ["Ready:", "9/10/2012"],
+		"isPower" 		: ["Power?", "Yes"],
+		"isWhiteLock" 	: ["Whitelock?", "Yes"],
+		"condition" 	: ["Condition:", "50"],
+		"comments"		: ["Additional Comments:", "Apartment needs to be trashed out, alot of things left behind 10 bags plus."]
+	},
+	"apartment17": {
+		"aptType" 		: ["Type:", "Vacant Available"],
+		"aptNum"  		: ["Apartment Number:", "2117"],
+		"aptSize" 		: ["Apartment Size:", "2x2"],
+		"vacDate" 		: ["Vacate:", "8/30/2012"],
+		"rdyDate" 		: ["Ready:", "9/15/2012"],
+		"isPower" 		: ["Power?", "Yes"],
+		"isWhiteLock" 	: ["Whitelock?", "Yes"],
+		"condition" 	: ["Condition:", "100"],
+		"comments"		: ["Additional Comments:", "Apartment is completed."]
+	},
+	"apartment18": {
+		"aptType" 		: ["Type:", "Vacant Available"],
+		"aptNum"  		: ["Apartment Number:", "1118"],
+		"aptSize" 		: ["Apartment Size:", "2x2"],
+		"vacDate" 		: ["Vacate:", "8/30/2012"],
+		"rdyDate" 		: ["Ready:", "9/5/2012"],
+		"isPower" 		: ["Power?", "Yes"],
+		"isWhiteLock" 	: ["Whitelock?", "Yes"],
+		"condition" 	: ["Condition:", "100"],
+		"comments"		: ["Additional Comments:", "Easy turn."]
+	},
+	"apartment19": {
+		"aptType" 		: ["Type:", "Vacant Available"],
+		"aptNum"  		: ["Apartment Number:", "2136"],
+		"aptSize" 		: ["Apartment Size:", "1x1"],
+		"vacDate" 		: ["Vacate:", "9/3/2012"],
+		"rdyDate" 		: ["Ready:", "9/10/2012"],
+		"isPower" 		: ["Power?", "Yes"],
+		"isWhiteLock" 	: ["Whitelock?", "Yes"],
+		"condition" 	: ["Condition:", "100"],
+		"comments"		: ["Additional Comments:", "Apartment is completed."]
+	},
+	"apartment20": {
+		"aptType" 		: ["Type:", "Vacant Rented"],
+		"aptNum"  		: ["Apartment Number:", "1111"],
+		"aptSize" 		: ["Apartment Size:", "1x1"],
+		"vacDate" 		: ["Vacate:", "8/30/2012"],
+		"rdyDate" 		: ["Ready:", "9/15/2012"],
+		"isPower" 		: ["Power?", "Yes"],
+		"isWhiteLock" 	: ["Whitelock?", "Yes"],
+		"condition" 	: ["Condition:", "100"],
+		"comments"		: ["Additional Comments:", "Apartment is completed."]
 	}
 };
